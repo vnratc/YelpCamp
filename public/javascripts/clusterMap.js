e9e76916306cb7126a79df9d4c46314f028f1d9c
vnratc <105481726+vnratc@users.noreply.github.com>
2023-07-18 11:11:17 +0400

    center to Garni

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -13,7 +13,8 @@ const map = new mapboxgl.Map({
   container: 'cluster-map',
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: 'mapbox://styles/mapbox/light-v11',
-  center: [-103.5917, 40.6699],
+  // center: [-103.5917, 40.6699],
+  center: [40.113918, 44.731685],
   zoom: 3
 });
 
