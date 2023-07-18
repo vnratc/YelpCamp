7257b31eb1dd1d5e56b6e6ff621394e541174a0b
vnratc <105481726+vnratc@users.noreply.github.com>
2023-07-18 10:10:50 +0400

    before deploying with Render

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== "production") {
     require("dotenv").config()
 }
 
+
 // Express.
 const express = require("express")
 const app = express()
@@ -13,15 +14,37 @@ const path = require("path")
 const mongoose = require("mongoose")
 
 // Template engine.
-const ejsMate = require("ejs-mate") 
+const ejsMate = require("ejs-mate")
 const session = require("express-session")
 const flash = require("connect-flash")
 
+// MongoDB Atlas
+const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp"
+// const dbUrl = process.env.DB_URL
+
+
+// Storing session in Mongo
+// const MongoDBStore = require("connect-mongo")(session)
+const MongoStore = require('connect-mongo');
+const store = MongoStore.create({
+    // mongoUrl: dbUrl,
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'thisshouldbeabettersecret!'
+    }
+});
+
+store.on("error", function(e) {
+    console.log("SESSION STORE ERROR", e)
+} )
+
+
 // Error class.
-const ExpressError = require("./utils/ExpressError") 
+const ExpressError = require("./utils/ExpressError")
 
 // Different request types.
-const methodOverride = require("method-override") 
+const methodOverride = require("method-override")
 
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
@@ -34,6 +57,12 @@ const reviewRoutes = require("./routes/reviews")
 const userRoutes = require("./routes/users")
 
 
+
+// Protect headers
+const helmet = require("helmet");
+const { options } = require("joi");
+
+
 // Connect Mongoose database.
 main()
     .then(() => console.log("Mongoose connection Open"))
@@ -43,36 +72,39 @@ async function main() {
     // "true" prevents information that is not in schema fields from being saved to the database, "false" allows it.
     mongoose.set('strictQuery', true)
 
-    await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp")
+    await mongoose.connect(dbUrl)
 }
 
 
 // Set a derictory for the application's views and a default engine to use.
-app.set("views", path.join(__dirname, "views")) 
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
 // Register template engine.
-app.engine("ejs", ejsMate) 
+app.engine("ejs", ejsMate)
 
 // Import ability to extract form data (parse req.body)
-app.use(express.urlencoded({ extended: true })) 
+app.use(express.urlencoded({ extended: true }))
 
 // Add different request types
-app.use(methodOverride("_method")) 
+app.use(methodOverride("_method"))
 
 // Static files. __dirname is required to be able run "nodemon app.js" from any working directory
-app.use(express.static(path.join(__dirname, "public"))) 
+app.use(express.static(path.join(__dirname, "public")))
 
 
 // Use session.
 const sessionConfig = {
+    store,
+    name: 'session',
     secret: "thisshouldbeabettersecret",
     resave: false,
     saveUninitialized: true,
     cookie: {
         expires: Date.now() + 1000 * 3600 * 24 * 7, // week
         maxAge: 1000 * 3600 * 24 * 7, // week
-        httpOnly: true
+        httpOnly: true,
+        // secure: true  // Require only https, not just http
     }
 }
 app.use(session(sessionConfig)) // This must be before passport.session
@@ -90,21 +122,71 @@ passport.deserializeUser(User.deserializeUser())
 // Use flash.
 app.use(flash())
 
-app.use(mongoSanitize({replaceWith: 'kuku'}))
+
+// DOES NOT WORK AS IN LECTURE. DISABLING Content-Security-Policy
+app.use(helmet({ contentSecurityPolicy: false }))
+// // List allowed sources for helmet
+// const scriptSrcUrls = [
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://api.mapbox.com/",
+//     "https://kit.fontawesome.com/",
+//     "https://cdnjs.cloudflare.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+// // This is the array that needs added to
+// const styleSrcUrls = [
+//     "https://kit-free.fontawesome.com/",
+//     "https://api.mapbox.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://fonts.googleapis.com/",
+//     "https://use.fontawesome.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+// const connectSrcUrls = [
+//     "https://api.mapbox.com/",
+//     "https://a.tiles.mapbox.com/",
+//     "https://b.tiles.mapbox.com/",
+//     "https://events.mapbox.com/",
+// ];
+// const fontSrcUrls = [];
+// // Use helmet with allowed sources defined above
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//             workerSrc: ["'self'", "blob:"],
+//             objectSrc: [],
+//             imgSrc: [
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://res.cloudinary.com/drzyxwo23/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+//                 "https://images.unsplash.com/",
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls],
+//         },
+//     })
+// );
+
+app.use(mongoSanitize({ replaceWith: 'kuku' }))
 
 // Use local variables.
 app.use((req, res, next) => { // THIS MUST BE BEFORE ROUTES
-    
+
     // console.log(req.query)
 
     // .currentUser is now available in templates.
     res.locals.currentUser = req.user
 
     // .success is array that's available in templates
-    res.locals.success = req.flash("success") 
+    res.locals.success = req.flash("success")
 
     // "res.locals" property sets variables accessible in templates rendered with res.render. The variables set on res.locals are available within a single request-response cycle.
-    res.locals.error = req.flash("error") 
+    res.locals.error = req.flash("error")
     next()
 })
 
--- joiSchemas.js
+++ joiSchemas.js
@@ -1,24 +1,48 @@
-const Joi = require("joi")
+const BaseJoi = require("joi")
+const sanitizeHtml = require('sanitize-html')
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    'string.escapeHTML': '{{#label}} must not include HTML!'
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value) return helpers.error('string.escapeHTML', { value })
+        return clean
+      }
+    }
+  }
+})
+
+
+const Joi = BaseJoi.extend(extension)
 
 
 // JOI data validation. Define a validation schema
 // Shorter way of exporting without creating a variable. We just add property to module.exports.
 module.exports.campgroundValidationSchema = Joi.object({
-    // campground because form names are "campground[title]...", i.e. we store form names in additional object
-    campground: Joi.object({
-        title: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        // image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
-    }).required(),
-    deleteImages: Joi.array()    
+  // campground because form names are "campground[title]...", i.e. we store form names in additional object
+  campground: Joi.object({
+    title: Joi.string().required().escapeHTML(),
+    price: Joi.number().required().min(0),
+    // image: Joi.string().required(),
+    location: Joi.string().required().escapeHTML(),
+    description: Joi.string().required()
+  }).required(),
+  deleteImages: Joi.array()
 })
 
 
 module.exports.reviewValidationSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
-    }).required()
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    body: Joi.string().required().escapeHTML()
+  }).required()
 })
